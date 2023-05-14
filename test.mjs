1b8265c370918ea0a70f149c54dea5540a567581
song <281218023@qq.com>
2023-05-14 19:40:32 +0800

    feat: OrangeGpio test

--- test.mjs
+++ test.mjs
@@ -1,11 +1,15 @@
 import { GpioControl } from './index.js'
 
-const GPIO = { PC: 'PC' }
+const group = { PC: 'PC' }
 
+// const register = {
+//   CFG:
+// }
 const config = {
   PC: {
     base_address: 0x0300b000,
     base_offset: 2 * 0x0024 + 0,
+
     pins: {
       5: {
         DAT: {
@@ -163,18 +167,50 @@ const config = {
     },
   },
 }
+function setBitVal(number, startBit, val, length = 1) {
+  var mask = (1 << length) - 1 // 生成掩码，将要修改的位数全部变为1
+  mask <<= startBit // 将掩码移动到起始位
+  val <<= startBit // 将要设置的值移动到起始位，支持二进制、十进制、十六进制输入
+  val &= mask // 通过掩码限制val的取值范围
+  if (val) {
+    number |= mask // 将掩码应用到原数上，将要修改的位数全部变为1
+    number &= ~(mask ^ val) // 将掩码和要设置的值应用到原数上
+  } else {
+    mask = ~mask // 取反掩码，将要修改的位数全部变为0
+    number &= mask // 将掩码应用到原数上，将对应位变为0
+  }
+  return number
+}
+class OrangeGpio {
+  constructor(group) {
+    if (config[group]) {
+      this.group = group
+      this.GpioControl = new GpioControl(
+        config[group].base_address,
+        config[group].base_offset
+      )
+    }
+  }
+  setMode(pin, modeCode) {
+    const offset = Math.floor(pin / 8)
+    const current = this.GpioControl.getVal(offset)
+    this.GpioControl.setVal(offset, setBitVal(current, pin % 8, modeCode, 3))
+  }
+  setVal(pin, val) {
+    const offset = 4
+    const current = this.GpioControl.getVal(offset)
+    this.GpioControl.setVal(offset, setBitVal(current, pin, val))
+  }
+}
+// const gpio = new GpioControl(config.PC.base_address, config.PC.base_offset)
 
-// class OrangeGpio {
-//   constructor(GPIO) {
-//     if (config[GPIO]) {
-//       const gpio = new GpioControl(
-//         config.PC.base_address,
-//         config.PC.base_offset
-//       )
-//     }
-//   }
-// }
-const gpio = new GpioControl(config.PC.base_address, config.PC.base_offset)
-gpio.setVal(config.PC.CFG.offset, gpio.getVal(0) & 0x77777717)
-gpio.setVal(1, gpio.getVal(1) & 0x77777717)
-gpio.setVal(4, gpio.getVal(4) ^ 0x00003000)
+// gpio.setVal(config.PC.CFG.offset, gpio.getVal(0) & 0x77777717)
+// gpio.setVal(1, gpio.getVal(1) & 0x77777717)
+// gpio.setVal(4, gpio.getVal(4) ^ 0x00003000)
+// console.log(setBitVal(0b11110000, 0, 1, 3).toString(2))
+
+// const PC = new OrangeGpio('PC')
+// PC.setMode(1, 'xxx')
+// PC.setPull(1, 'xxx')
+// PC.setInt(1, 'xxx')
+// PC.setVal(1, 0)
